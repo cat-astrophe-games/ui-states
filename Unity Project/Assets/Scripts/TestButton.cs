9312c78605521b32c315672d97b92f13d88e869c
Michalis Kamburelis <michalis.kambi@gmail.com>
2020-05-12 22:30:54 +0200

    3 UI states designed for demo (main menu, options, game) and dummy game (FPS controller walking on plane)

---
+++ Unity Project/Assets/Scripts/TestButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        RectTransform t = transform as RectTransform;
+        //Debug.Log("Button " + name + "size is " + t.sizeDelta);
+        Debug.Log(string.Format("Button {0} size is {1}, anchored position {2}", name, t.sizeDelta, t.anchoredPosition));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
